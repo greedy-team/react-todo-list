c2237a79195244bd700e056cfe14e026524a0da7
songhj10207@naver.com <songhj10207@naver.com>
2025-02-05 13:05:49 +0900

    refactor: TodoInsert 버튼 타입 변경

--- src/components/Button/TodoAddButton.jsx
+++ src/components/Button/TodoAddButton.jsx
@@ -2,10 +2,8 @@ import { MdAdd } from 'react-icons/md';
 import TodoButton from './Todo/TodoButton';
 import colors from '../../color/color';
 
-const TodoAddButton = ({ onAddTodo }) => {
-  return (
-    <TodoButton onClick={onAddTodo} icon={MdAdd} iconColor={colors.green} />
-  );
+const TodoAddButton = () => {
+  return <TodoButton type="submit" icon={MdAdd} iconColor={colors.green} />;
 };
 
 export default TodoAddButton;
