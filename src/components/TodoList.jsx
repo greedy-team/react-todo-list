1c78dba7a9ff96fc0335a90d6cfd9ce2daeae48b
gxuoo <rbdud189@naver.com>
2025-07-16 17:00:13 +0900

    feat: TodoList, TodoListItem 컴포넌트 작성 및 App.jsx에 적용

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,25 @@
+import TodoListItem from "./TodoListItem";
+import styled from "styled-components";
+
+const TodoListContainer = styled.div`
+  min-height: 320px;
+  max-height: 513px;
+  overflow-y: auto;
+`;
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <TodoListContainer>
+      {todos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </TodoListContainer>
+  );
+};
+
+export default TodoList;
