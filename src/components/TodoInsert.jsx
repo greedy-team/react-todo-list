ac0cc40149bc10696bb35f693cacb28076ff14f5
Shin Jihoon <tobeabestns@naver.com>
2025-07-18 17:37:57 +0900

    feat: TodoInsert 컴포넌트 생성. Todo 내용을 입력 받고 상태를 추가하는 역할

--- src/components/TodoInsert.jsx
+++ src/components/TodoInsert.jsx
@@ -1,1 +1,41 @@
-// 새로운 항목을 입력하고 추가할 수 있는 컴포넌트. state를 통해서 인풋의 상태를 관리합니다.
+import { useState } from "react";
+import { InsertButton, InsertForm, StyledInput } from "./TodoInsert.styled";
+import useTodosStore from "../stores/todoStore";
+import { MdAdd } from "react-icons/md";
+
+function TodoInsert() {
+  const [value, setValue] = useState("");
+  const addTodo = useTodosStore((state) => state.addTodo);
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+
+    if (value.trim() === "") {
+      return alert("할 일을 입력해주세요!");
+    }
+
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <>
+      <InsertForm onSubmit={handleAddTodo}>
+        <StyledInput
+          placeholder="할 일을 입력하세요"
+          value={value}
+          onChange={handleInputChange}
+        />
+        <InsertButton type="submit">
+          <MdAdd />
+        </InsertButton>
+      </InsertForm>
+    </>
+  );
+}
+
+export default TodoInsert;
