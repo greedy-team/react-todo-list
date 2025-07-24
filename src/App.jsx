09180425b0cd561741842da0aab9997ab67f8c43
gxuoo <rbdud189@naver.com>
2025-07-24 15:45:27 +0900

    feat: 최적화 체크를 위한 BulkTodos 함수 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 import { useState } from "react";
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i < 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(createBulkTodos());
 
   const addTodoItem = (text) => {
     if (text === "") {
