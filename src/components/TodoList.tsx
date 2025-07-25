769e1717beb6605dc4e72e0e5b96357ff882f513
Shin Jihoon <tobeabestns@naver.com>
2025-07-25 18:36:21 +0900

    refactor: TodoList에 react-virtualized 적용

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import { TodoListBlock } from "./TodoList.styled.ts";
 import TodoListItem from "./TodoListItem.tsx";
 import { Todo } from "../stores/todoStore.ts";
+import { List } from "react-virtualized";
 
 interface TodoListProps {
   todos: Todo[];
@@ -8,19 +9,37 @@ interface TodoListProps {
   onDeleteTodo: (id: number) => void;
 }
 
-function TodoList({ todos, onCheckedTodo, onDeleteTodo }: TodoListProps) {
-  return (
-    <TodoListBlock>
-      {todos.map((todo) => (
+const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  onCheckedTodo,
+  onDeleteTodo,
+}) => {
+  const rowRenderer = ({ index, key, style }) => {
+    const todo = todos[index];
+
+    return (
+      <div key={key} style={style}>
         <TodoListItem
-          key={todo.id}
           todo={todo}
           onCheckedTodo={onCheckedTodo}
           onDeleteTodo={onDeleteTodo}
         />
-      ))}
+      </div>
+    );
+  };
+
+  return (
+    <TodoListBlock>
+      <List
+        width={400}
+        height={600}
+        rowCount={todos.length}
+        rowHeight={50}
+        rowRenderer={rowRenderer}
+        overscanRowCount={10}
+      />
     </TodoListBlock>
   );
-}
+};
 
 export default TodoList;
