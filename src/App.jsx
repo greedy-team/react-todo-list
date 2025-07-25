fe78e9df78c0363ac197fddbbda6584b2c670b59
gxuoo <rbdud189@naver.com>
2025-07-25 18:56:24 +0900

    refactor: App.jsx 최적화
    useCallback Hook 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 function createBulkTodos() {
   const array = [];
@@ -18,7 +18,7 @@ function createBulkTodos() {
 function App() {
   const [todos, setTodos] = useState(createBulkTodos());
 
-  const addTodoItem = (text) => {
+  const addTodoItem = useCallback((text) => {
     if (text === "") {
       alert("할 일을 작성해주셔야 합니다.");
       return;
@@ -29,20 +29,20 @@ function App() {
       text,
       checked: false,
     };
-    setTodos([...todos, todo]);
-  };
+    setTodos((prevTodos) => [...prevTodos, todo]);
+  }, []);
 
-  const removeTodoItem = (id) => {
+  const removeTodoItem = useCallback((id) => {
     setTodos((todos) => todos.filter((todo) => todo.id !== id));
-  };
+  }, []);
 
-  const toggleChecked = (id) => {
+  const toggleChecked = useCallback((id) => {
     setTodos((todos) =>
       todos.map((todo) =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo
       )
     );
-  };
+  }, []);
 
   return (
     <>
