4d439c80adbf369f8f3aa1eaffe56d13fc32df64
dkr-sjr <rkdrjs4637@naver.com>
2026-01-14 14:47:44 +0900

    feat : todoList 삽입, 삭제, 체크 함수 작성

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,31 @@ import TodoInsert from './components/TodoInsert';
 function App() {
   const [todoList, setTodoList] = useState([]);
 
+  const addNewTodo = ( newTodoText ) => {
+    const newTodo = {
+      id: Date.now(),
+      text: newTodoText,
+      checked: false,
+    };
+    const newTodoList = [...todoList, newTodo ];
+    setTodoList(newTodoList);
+  };
+
+  const removeTodoById = ( id ) => {
+    const removedTodoList = todoList.filter( (todo) => (todo.id !== id));
+    setTodoList(removedTodoList);
+  };
+
+  const toggleTodoCheckedById = ( id ) => {
+    const changedTodoList = todoList.map(( todo ) => {
+      if (todo.id === id) {
+        return {...todo, checked: !todo.checked };
+      }
+      return todo;
+    });
+    setTodoList(changedTodoList);
+  };
+
   return (
     <MainContainer>
       <TodoTemplate>
