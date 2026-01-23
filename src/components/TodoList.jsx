442c729a9f68001a27d6e824ad7b966c2e1f69de
Yeryeong Kang <dpfud8786@gmail.com>
2026-01-23 22:03:10 +0900

    feat: react-virtualized 적용

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,46 @@
 import styled from "styled-components";
+import { AutoSizer, List } from "react-virtualized";
 import TodoListItem from "./TodoListItem";
 
+const LIST_HEIGHT = 513;
+const ROW_HEIGHT = 56;
+
 export default function TodoList({ todos, removeTodo, toggleTodo }) {
-  return (
-    <TodoListBlock>
-      {todos.map((todo) => (
+  const rowRenderer = ({ index, key, style }) => {
+    const todo = todos[index];
+
+    return (
+      <Row key={key} style={style}>
         <TodoListItem
-          key={todo.id}
           todo={todo}
           removeTodo={removeTodo}
           toggleTodo={toggleTodo}
         />
-      ))}
+      </Row>
+    );
+  };
+
+  return (
+    <TodoListBlock>
+      <AutoSizer disableHeight>
+        {({ width }) => (
+          <List
+            width={width}
+            height={LIST_HEIGHT}
+            rowCount={todos.length}
+            rowHeight={ROW_HEIGHT}
+            rowRenderer={rowRenderer}
+            overscanRowCount={8}
+          />
+        )}
+      </AutoSizer>
     </TodoListBlock>
   );
 }
 
 const TodoListBlock = styled.div`
-  max-height: 513px;
+  height: ${LIST_HEIGHT}px;
   min-height: 320px;
-  overflow-y: auto;
 `;
+
+const Row = styled.div``;
